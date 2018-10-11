4483c304b01faef64fd6d0b4131cc054464796af
tstillwell2k17 <tristanstillwell@gmail.com>
2018-10-10 23:16:04 -0500

    refactor: Move js from material-lambert.html

---
+++ materials/material-lambert.js
@@ -0,0 +1,21 @@
+        var scene = new THREE.Scene();
+		var ambientLight = new THREE.AmbientLight( 0xFFFFFF, 0.5 );
+		var light = new THREE.PointLight(0xFFFFFF, 0.5 );
+		scene.add( ambientLight );
+		scene.add( light );
+        camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( renderer.domElement );
+        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        var material = new THREE.MeshLambertMaterial({ color: 0x00FF00 } );
+        var cube = new THREE.Mesh( geometry, material );
+        scene.add( cube );
+        camera.position.z = 5;
+        var animate = function () {
+                requestAnimationFrame( animate );
+                cube.rotation.x += 0.01;
+                cube.rotation.y += 0.01;
+                renderer.render( scene, camera );
+            };
+        animate();
