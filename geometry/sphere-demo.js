bcbfd12ff1fbd5ae3e28efe440933377dc3b58bb
tstillwell2k17 <tristanstillwell@gmail.com>
2018-10-11 21:15:46 -0500

    refactor: Move js from sphere-demo.html

---
+++ geometry/sphere-demo.js
@@ -0,0 +1,17 @@
+        var scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( renderer.domElement );
+        var geometry = new THREE.SphereGeometry( 5, 32, 32 );
+        var material = new THREE.MeshBasicMaterial( {color: 0xFFFF00} );
+        var sphere = new THREE.Mesh( geometry, material );
+        scene.add( sphere );
+        camera.position.z = 20;
+        var animate = function () {
+                requestAnimationFrame( animate );
+                sphere.rotation.x += 0.01;
+                sphere.rotation.y += 0.01;
+                renderer.render( scene, camera );
+            };
+        animate();
