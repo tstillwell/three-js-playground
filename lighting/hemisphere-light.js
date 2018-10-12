47a2a95bc229c47ba9c0992be16e7dcd58c78c3a
tstillwell2k17 <tristanstillwell@gmail.com>
2018-10-11 21:01:02 -0500

    refactor: Move js from hemisphere-light.html

---
+++ lighting/hemisphere-light.js
@@ -0,0 +1,20 @@
+        var scene = new THREE.Scene();
+        var hemisphereLight = new THREE.HemisphereLight(0xFFFFFF,0xFF0000,1);
+        hemisphereLight.position.set(1, 0, 1).normalize();
+        scene.add(hemisphereLight);
+        camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( renderer.domElement );
+        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        var material = new THREE.MeshStandardMaterial();
+        var cube = new THREE.Mesh( geometry, material );
+        scene.add( cube );
+        camera.position.z = 5;
+        var animate = function () {
+                requestAnimationFrame( animate );
+                cube.rotation.x += 0.01;
+                cube.rotation.y += 0.01;
+                renderer.render( scene, camera );
+            };
+        animate();
