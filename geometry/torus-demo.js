c119a62a6de064abea64a75a0155dcbe4b67d9d3
tstillwell2k17 <tristanstillwell@gmail.com>
2018-10-11 21:10:31 -0500

    refactor: Move js from torus-demo.html

---
+++ geometry/torus-demo.js
@@ -0,0 +1,17 @@
+        var scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( renderer.domElement );
+        var geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+        var material = new THREE.MeshBasicMaterial( { color: 0xFFFF00 } );
+        var torus = new THREE.Mesh( geometry, material );
+        scene.add( torus );
+        camera.position.z = 50;
+        var animate = function () {
+                requestAnimationFrame( animate );
+                torus.rotation.y += 0.01;
+                torus.rotation.x += 0.01;
+                renderer.render( scene, camera );
+            };
+        animate();
