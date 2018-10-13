3997b3ef67fef0657f5baa093f49480415ceb1e6
tstillwell2k17 <tristanstillwell@gmail.com>
2018-10-12 23:55:58 -0500

    refactor: Move js from cylinder-demo.html

---
+++ geometry/cylinder-demo.js
@@ -0,0 +1,16 @@
+        var scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( renderer.domElement );
+        var geometry = new THREE.CylinderGeometry( 5, 5, 20, 32 );
+        var material = new THREE.MeshBasicMaterial( {color: 0xFFFF00} );
+        var cylinder = new THREE.Mesh( geometry, material );
+        scene.add( cylinder );
+        camera.position.z = 50;
+        var animate = function () {
+                requestAnimationFrame( animate );
+                cylinder.rotation.y += 0.5;
+                renderer.render( scene, camera );
+            };
+        animate();
