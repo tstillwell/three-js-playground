f787ee49ea7c0f64e48f75b411864256b4ea91af
tstillwell2k17 <tristanstillwell@gmail.com>
2018-10-12 23:54:19 -0500

    refactor: Move js from ellipse-curve.html

---
+++ geometry/ellipse-curve.js
@@ -0,0 +1,25 @@
+        var scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( renderer.domElement );
+        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        var material = new THREE.MeshBasicMaterial( { color: 0x00FF00 } );
+        var cube = new THREE.Mesh( geometry, material );
+        scene.add( cube );
+        var line_material = new THREE.LineBasicMaterial({color:0xFFFFFF, opacity:1});
+        var ellipse = new THREE.EllipseCurve(0, 0, 1, 5, 0, 2.0 * Math.PI, false);
+        var ellipsePath = new THREE.CurvePath();
+        ellipsePath.add(ellipse);
+        var ellipseGeometry = ellipsePath.createPointsGeometry(100);
+        ellipseGeometry.computeTangents();
+        var line = new THREE.Line(ellipseGeometry, line_material);
+        scene.add( line );
+        camera.position.z = 20;
+        var animate = function () {
+                requestAnimationFrame( animate );
+                cube.rotation.x += 0.01;
+                cube.rotation.y += 0.01;
+                renderer.render( scene, camera );
+            };
+        animate();
