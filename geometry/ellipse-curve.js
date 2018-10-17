66f505fe59864eaf525bde44fec23e56bda14c9e
tstillwell2k17 <tristanstillwell@gmail.com>
2018-10-16 21:26:32 -0500

    refactor: Remove curvePath instance from geometry.js

--- geometry/ellipse-curve.js
+++ geometry/ellipse-curve.js
@@ -9,8 +9,6 @@
         scene.add( cube );
         var line_material = new THREE.LineBasicMaterial({color:0xFFFFFF, opacity:1});
         var ellipse = new THREE.EllipseCurve(0, 0, 1, 5, 0, 2.0 * Math.PI, false);
-        var ellipsePath = new THREE.CurvePath();
-        ellipsePath.add(ellipse);
 		var ellipsePoints = ellipse.getPoints(100);
         var ellipseGeometry = new THREE.BufferGeometry().setFromPoints(ellipsePoints);
         var line = new THREE.Line(ellipseGeometry, line_material);
