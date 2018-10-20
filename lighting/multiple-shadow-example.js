af6ff588429dd908b2929ce736c81101315a4447
tstillwell2k17 <tristanstillwell@gmail.com>
2018-10-19 22:38:42 -0500

    feat: Add orbit controls to multiple-shadow-example

--- lighting/multiple-shadow-example.js
+++ lighting/multiple-shadow-example.js
@@ -8,7 +8,8 @@
         scene.add( directionalLight2 );
         directionalLight2.position.set( -10, 10, 10);
         camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-        renderer = new THREE.WebGLRenderer();
+        controls = new THREE.OrbitControls(camera);
+		renderer = new THREE.WebGLRenderer();
         renderer.setSize( window.innerWidth, window.innerHeight );
         renderer.shadowMapEnabled = true;
         document.body.appendChild( renderer.domElement );
