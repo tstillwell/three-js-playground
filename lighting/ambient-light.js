387e540d87ca407713625b51493098c01e46f850
tstillwell2k17 <tristanstillwell@gmail.com>
2018-10-19 22:34:26 -0500

    feat: Add orbit controls to ambient-light demo

--- lighting/ambient-light.js
+++ lighting/ambient-light.js
@@ -2,7 +2,8 @@
         var ambientLight = new THREE.AmbientLight( 0x333333 );
         scene.add( ambientLight );
         camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
-        renderer = new THREE.WebGLRenderer();
+        controls = new THREE.OrbitControls(camera);
+		renderer = new THREE.WebGLRenderer();
         renderer.setSize( window.innerWidth, window.innerHeight );
         document.body.appendChild( renderer.domElement );
         var geometry = new THREE.BoxGeometry( 1, 1, 1 );
