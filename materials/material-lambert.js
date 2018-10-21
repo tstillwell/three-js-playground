4155a20d36739433783e43ef5bb13f5215ec5add
tstillwell2k17 <tristanstillwell@gmail.com>
2018-10-20 20:47:09 -0500

    feat: Add orbit controls to material-lambert example

--- materials/material-lambert.js
+++ materials/material-lambert.js
@@ -4,7 +4,8 @@
 		scene.add( ambientLight );
 		scene.add( light );
         camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
-        renderer = new THREE.WebGLRenderer();
+        controls = new THREE.OrbitControls(camera);
+		renderer = new THREE.WebGLRenderer();
         renderer.setSize( window.innerWidth, window.innerHeight );
         document.body.appendChild( renderer.domElement );
         var geometry = new THREE.BoxGeometry( 1, 1, 1 );
