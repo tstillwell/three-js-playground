9c67449386d0e791a41649a13c7c6cc9c57c9f3f
tstillwell2k17 <tristanstillwell@gmail.com>
2018-10-22 22:03:11 -0500

    feat: Add mui frontend for viewing examples

--- geometry/geometry.js
+++ geometry/geometry.js
@@ -18,16 +18,16 @@ function addDemoListeners(){
 function displayDemo(){
     let demo_pane = document.getElementById("demo-pane");
     const demo_files = [
-        { id: 'cone', src: "cone-demo.html"},
-        { id: 'cube', src: "cube-demo.html"},
-        { id: 'cylinder', src: "cylinder-demo.html"},
-        { id: 'ellipse-curve', src: "ellipse-curve.html"},
-        { id: 'plane', src: "plane-demo.html"},
-        { id: 'plane-wireframe', src: "plane-wireframe-demo.html"},
-        { id: 'ring', src: "ring-demo.html"},
-        { id: 'sphere', src: "sphere-demo.html"},
-        { id: 'tetrahedron', src: "tetrahedron-demo.html"},
-        { id: 'torus', src: "torus-demo.html"},
+        { id: 'cone', src: "geometry/cone-demo.html"},
+        { id: 'cube', src: "geometry/cube-demo.html"},
+        { id: 'cylinder', src: "geometry/cylinder-demo.html"},
+        { id: 'ellipse-curve', src: "geometry/ellipse-curve.html"},
+        { id: 'plane', src: "geometry/plane-demo.html"},
+        { id: 'plane-wireframe', src: "geometry/plane-wireframe-demo.html"},
+        { id: 'ring', src: "geometry/ring-demo.html"},
+        { id: 'sphere', src: "geometry/sphere-demo.html"},
+        { id: 'tetrahedron', src: "geometry/tetrahedron-demo.html"},
+        { id: 'torus', src: "geometry/torus-demo.html"},
     ];
     for (file in demo_files) {
         if (demo_files[file].id == this.id) {
@@ -36,4 +36,13 @@ function displayDemo(){
     }
 }
 
+function iframeLoaded() {
+    const iFrameID = document.getElementById('demo-pane');
+    if(iFrameID) {
+      iFrameID.height = "";
+      iFrameID.height = document.body.scrollHeight + "px";
+    }
+}
+
+iframeLoaded();
 addDemoListeners();
