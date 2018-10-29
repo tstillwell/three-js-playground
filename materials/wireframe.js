b7e2f7633c6f1b5f2ccdad834ebf7535c79a4ff9
tstillwell2k17 <tristanstillwell@gmail.com>
2018-10-28 22:23:49 -0500

    feat: Make wireframe demo camera perspective consistent

--- materials/wireframe.js
+++ materials/wireframe.js
@@ -1,5 +1,5 @@
         var scene = new THREE.Scene();
-        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
         controls = new THREE.OrbitControls(camera);
         renderer = new THREE.WebGLRenderer();
         renderer.setSize( window.innerWidth, window.innerHeight );
