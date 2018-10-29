1bd3fd74269a70a8b4dad03f4abce699ff82e48b
tstillwell2k17 <tristanstillwell@gmail.com>
2018-10-28 22:26:37 -0500

    style: Move 'camera' line in material-phong for consistency

--- materials/material-phong.js
+++ materials/material-phong.js
@@ -1,8 +1,8 @@
         var scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
         var hemisphereLight = new THREE.HemisphereLight(0xFFFFFF,0xFF0000,1);
         hemisphereLight.position.set(1, 0, 1).normalize();
         scene.add(hemisphereLight);
-        camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
         controls = new THREE.OrbitControls(camera);
         renderer = new THREE.WebGLRenderer();
         renderer.setSize( window.innerWidth, window.innerHeight );
