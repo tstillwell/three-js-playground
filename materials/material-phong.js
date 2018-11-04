238419bd0d5954fd937e1b556514939099de557e
tstillwell2k17 <tristanstillwell@gmail.com>
2018-11-03 22:13:08 -0500

    style: Remove extra indentation level from materials scripts

--- materials/material-phong.js
+++ materials/material-phong.js
@@ -1,25 +1,25 @@
 function loadDemo(){
-        var scene = new THREE.Scene();
-        camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
-        var hemisphereLight = new THREE.HemisphereLight(0xFFFFFF,0xFF0000,1);
-        hemisphereLight.position.set(1, 0, 1).normalize();
-        scene.add(hemisphereLight);
-        controls = new THREE.OrbitControls(camera);
-        renderer = new THREE.WebGLRenderer();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        document.body.appendChild( renderer.domElement );
-        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        var material = new THREE.MeshPhongMaterial();
-        var cube = new THREE.Mesh( geometry, material );
-        scene.add( cube );
-        camera.position.z = 5;
-        var animate = function () {
-                requestAnimationFrame( animate );
-                cube.rotation.x += 0.01;
-                cube.rotation.y += 0.01;
-                renderer.render( scene, camera );
-            };
-        animate();
+    var scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    var hemisphereLight = new THREE.HemisphereLight(0xFFFFFF,0xFF0000,1);
+    hemisphereLight.position.set(1, 0, 1).normalize();
+    scene.add(hemisphereLight);
+    controls = new THREE.OrbitControls(camera);
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshPhongMaterial();
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+    camera.position.z = 5;
+    var animate = function () {
+            requestAnimationFrame( animate );
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+            renderer.render( scene, camera );
+        };
+    animate();
 }
 
 loadDemo();
