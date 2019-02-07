a7cdab9a6f34eb749dd069fe3bbd78124e120448
tstillwell <tristanstillwell@gmail.com>
2019-02-07 15:09:28 -0700

    style: Add space before opening brace in cone-demo

--- geometry/cone-demo.js
+++ geometry/cone-demo.js
@@ -1,4 +1,4 @@
-function loadDemo(){
+function loadDemo() {
     var scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
     controls = new THREE.OrbitControls(camera);
